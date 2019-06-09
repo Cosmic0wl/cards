ca11ac44279a02d95fc8b98e515b4b64b2771639
Cosmic0wl <valentinaspanetta@gmail.com>
2019-06-09 19:02:23 +0200

    finished script

--- script.js
+++ script.js
@@ -1,2 +1,58 @@
-// generate 10 cards
+$(document).ready(function() {
 
+    // generate 10 cards
+    var cardsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var shuffledArray = [];
+
+    function generateCards(array) {
+        var cloneArray = array.slice(0);
+        for (let i = 0; i < 10; i++) {
+            var randomNum = Math.random() * (9 - i);
+            var cardIndex = Math.round(randomNum);
+            shuffledArray.push(cloneArray.splice(cardIndex, 1));
+        }
+        return shuffledArray;
+    }
+    generateCards(cardsArray);
+    // print player cards
+    for (i = 0; i < cardsArray.length; i++) {
+        var player = $(".player");
+        var pCard = cardsArray[i];
+        $(player[i]).text(pCard);
+    }
+    // print computer cards
+    for (i = 0; i < shuffledArray.length; i++) {
+        var computer = $(".computer");
+        var cCard = shuffledArray[i];
+        $(computer[i]).text(cCard);
+    }
+
+
+
+     // drag function
+    $(function() {
+        $(".draggable").draggable();
+        $(".droppable").droppable({
+            drop: function(event, ui) {
+            	var playerCard = $(ui.draggable).text();
+            	var computerCard = $(this).text();
+            	console.log(playerCard);
+            	console.log(computerCard);
+                if (playerCard == computerCard) {
+                    $(this).addClass("alert-success").find("p").removeClass("hidden");
+                    $(ui.draggable).removeClass("alert-dark").addClass("alert-success");
+                    var position = $(this).position();
+                    console.log(position);
+                    $(this).css({"position":"relative"});
+                    $(ui.draggable).css({"position":"absolute"});
+                } else {
+                	$(this).addClass("alert-danger").find("p").removeClass("hidden");
+                    $(ui.draggable).addClass("alert-dark");
+                    $(ui.draggable).css({"top":"0", "left":"0"});
+                    
+                }
+
+            }
+        });
+    });
+});
